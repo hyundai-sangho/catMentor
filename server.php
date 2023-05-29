bb1e89ebd13bad444f6f34d70d43d6d193e7c029
조상호 <hyundai_sangho@naver.com>
2023-05-29 21:26:15 +0900

    주석 한글로 변경

--- server.php
+++ server.php
@@ -1,21 +1,21 @@
 <?php
 
 /**
- * Laravel - A PHP Framework For Web Artisans
+ * Laravel - 웹 장인을 위한 PHP 프레임워크
  *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @package 라라벨
+ * @author 테일러 오트웰 <taylor@laravel.com>
  */
 
 $uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+  parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+  return false;
 }
 
-require_once __DIR__.'/public/index.php';
+require_once __DIR__ . '/public/index.php';
