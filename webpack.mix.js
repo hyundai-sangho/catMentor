1314446f2b2b63b9b06a7b2ed993329e9f47f01b
조상호 <hyundai_sangho@naver.com>
2023-06-01 18:20:42 +0900

    주석 한글로 변환

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,20 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
+ | 혼합 자산 관리
+ |------------------------------------------------- -------------------------
  |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
+ | Mix는 일부 Webpack 빌드 단계를 정의하기 위한 깨끗하고 유창한 API를 제공합니다.
+ | 당신의 라라벨 애플리케이션을 위해. 기본적으로 CSS를 컴파일하고 있습니다.
+ | 애플리케이션용 파일과 모든 JS 파일을 번들로 묶습니다.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [
         //
-    ]);
+    ]
+);
